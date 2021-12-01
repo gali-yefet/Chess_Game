aebb39735fc7ff6961a553f698517667299720fc
eran friedman-merin <eran.fr2018@leyada.net>
2021-12-01 16:18:01 +0200

    king valadation

---
+++ chessGame/King.cpp
@@ -0,0 +1,13 @@
+#include "King.h"
+King::King(const int x, const int y, const int color, const string type, GameStatus* board)
+    :Piece(x, y, color, type, board)
+{}
+
+int King::valadateMove(const int newX, const int newY)
+{
+    if (((newY - getY() >= -1) && (newY - getY() <= 1)) && ((newX - getX() >= -1) && (newX - getX() <= 1)))
+    {
+        return 0;
+    }
+    return 6;
+}
--- chessGame/King.h
+++ chessGame/King.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "Piece.h"
+class King : public Piece
+{
+	King(const int x, const int y, const int color, const string type, GameStatus* board);
+	int valadateMove(const int newX, const int newY)override;
+};
