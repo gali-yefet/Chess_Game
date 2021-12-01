6a3d0b847c010b28fa5720e1402edc57ffe76918
eran friedman-merin <eran.fr2018@leyada.net>
2021-12-01 14:07:07 +0200

    regular piece done

---
+++ chessGame/Piece.cpp
@@ -0,0 +1,85 @@
+#include "Piece.h"
+/*moves the piece
+* input: x, y
+* output: none
+*/
+void Piece::move(const int x, const int y)
+{
+	setX(x);
+	setY(y);
+}
+/*set x
+* input: x
+* output: none
+*/
+void Piece::setX(const int x)
+{
+	_posX = x;
+}
+/*set y
+* input: y
+* output: none
+*/
+void Piece::setY(const int y)
+{
+	_posY = y;
+}
+/*set color
+* input: color
+* output: none
+*/
+void Piece::setColor(const int color)
+{
+	_color = color;
+}
+/*set type
+* input: type
+* output: none
+*/
+void Piece::setType(const string type)
+{
+	_type = type;
+}
+/*get x
+* input: none
+* output: x
+*/
+int Piece::getX()const
+{
+	return _posX;
+}
+/*get y
+* input: none
+* output: y
+*/
+int Piece::getY()const
+{
+	return _posY;
+}
+/*get color
+* input: none
+* output: color
+*/
+int Piece::getColor()const
+{
+	return _color;
+}
+/*get type
+* input: none
+* output: type
+*/
+string Piece::getType()const
+{
+	return _type;
+}
+/*constructor
+* input: x, y, color and type
+* output: none
+*/
+Piece::Piece(const int x, const int y, const int color, const string type)
+{
+	_posX = x;
+	_posY = y;
+	_color = color;
+	_type = type;
+}
--- chessGame/Piece.h
+++ chessGame/Piece.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "GameStatus.h"
+#include <iostream>
+using namespace std;
 class Piece
 {
 public:
+	Piece(const int x, const int y, const int color, const string type);
 	void move(const int x, const int y);
+	virtual int valadateMove(const int newX, const int newY /*,GameStatus* board*/) = 0;
+	int getX()const;
+	int getY()const;
+	int getColor()const;
+	string getType()const;
+	void setX(const int x);
+	void setY(const int y);
+	void setColor(const int color);
+	void setType(const string type);
+
+private:
+	int _color;
+	string _type;
+	int _posX;
+	int _posY;
 };
