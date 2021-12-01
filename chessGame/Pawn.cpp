99ddc11cf33ba766239e486391af09a12bd72b03
eran friedman-merin <eran.fr2018@leyada.net>
2021-12-01 17:32:59 +0200

    Pawn valadation

---
+++ chessGame/Pawn.cpp
@@ -0,0 +1,31 @@
+#include "Pawn.h"
+Pawn::Pawn(const int x, const int y, const int color, const string type, GameStatus* board)
+	:Piece(x, y, color, type, board)
+{}
+
+
+
+
+int Pawn::valadateMove(const int newX, const int newY)
+{
+	if (((getColor()==0 && newY-getY()==-1) || (getColor() != 0 && newY - getY() == 1))&& getX()==newX)
+	{
+		return 0;
+	}
+	if (((getColor() == 0 && newY - getY() == -1 && getY() == 7) || (getColor() != 0 && newY - getY() == 1 && getY() == 2)) && getX() == newX)
+	{
+		return 0;
+	}
+	if (((getColor() == 0 && newY - getY() == -1) || (getColor() != 0 && newY - getY() == 1)) && ((newX - getX() == -1)||(newX-getX()==1)))
+	{
+		for (int i = 0; i < getGame()->getBoard().size(); i++)
+		{
+			if (getGame()->getBoard()[i]->getX() == newX && getGame()->getBoard()[i]->getY() == newY)
+			{
+				return 0;
+			}
+		}
+		return 6;
+	}
+	return 6;
+}
--- chessGame/Pawn.h
+++ chessGame/Pawn.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "Piece.h"
+class Pawn : public Piece
+{
+	Pawn(const int x, const int y, const int color, const string type, GameStatus* board);
+	int valadateMove(const int newX, const int newY)override;
+};
