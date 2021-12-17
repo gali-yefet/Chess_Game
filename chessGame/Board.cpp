0e1c4ed6cc843f975a5f34b33da61e688721e96d
eran friedman-merin <eran.fr2018@leyada.net>
2021-12-17 21:40:13 +0200

    correction for commit

--- chessGame/Board.cpp
+++ chessGame/Board.cpp
@@ -1,6 +1,6 @@
 #include "Board.h"
 
-enum messages { VALID, CHECK, NO_P, DES_P, CHECK_C, RANGE, I_MOVE, SAME, CHECKMATE };
+enum messages{VALID, CHECK, NO_P, DES_P, CHECK_C, RANGE, I_MOVE, SAME, CHECKMATE};
 
 Board::Board()
 {
@@ -17,7 +17,7 @@ int Board::valadate(int x, int y, int newX, int newY)
 	int res = VALID; //return value
 
 	//getting index of piece in the source point
-	//if the piece is not of the current player i == board.size
+	//if the piece is of the current player i == board.size
 	while (i < this->game->getBoard().size() && flag)
 	{
 		if (((this->game->getBoard()[i]->getX() == x) && (this->game->getBoard()[i]->getY() == y)) && ((this->game->getIsWhiteTurn() == false && this->game->getBoard()[i]->getColor() != 0) || (this->game->getIsWhiteTurn() == true && this->game->getBoard()[i]->getColor() == 0)))
@@ -68,7 +68,7 @@ int Board::valadate(int x, int y, int newX, int newY)
 	flag = true;
 	while (kCurrent < this->game->getBoard().size() && flag)
 	{
-		if (this->game->getBoard()[kCurrent]->getType() == "king" && (this->game->getBoard()[k]->getColor() == this->game->getBoard()[i]->getColor()))
+		if (this->game->getBoard()[kCurrent]->getType() == "king" && (this->game->getBoard()[kCurrent]->getColor() == this->game->getBoard()[i]->getColor()))
 		{
 			flag = false;// the index of the current player's king found
 		}
@@ -80,7 +80,7 @@ int Board::valadate(int x, int y, int newX, int newY)
 
 	//check options
 
-
+	
 	if (j != this->game->getBoard().size() && this->game->getBoard()[j]->getColor() == this->game->getBoard()[i]->getColor())// the player has piece at the destination point 
 	{
 		return DES_P;
@@ -97,15 +97,7 @@ int Board::valadate(int x, int y, int newX, int newY)
 	{
 		return  SAME;
 	}
-	if (i == this->game->getBoard().size())// the player has no piece at the source point 
-	{
-		res = NO_P;
-	}
-	if (j != this->game->getBoard().size() && this->game->getBoard()[j]->getColor() == this->game->getBoard()[i]->getColor())// the player has piece at the destination point 
-	{
-		res = DES_P;
-	}
-
+	
 	this->game->getBoard()[i]->move(newX, newY);// moving the current piece to the destination
 
 	for (int a = 0; a < this->game->getBoard().size(); a++)
@@ -117,9 +109,9 @@ int Board::valadate(int x, int y, int newX, int newY)
 		}
 	}
 
-	for (int a = 0; a < this->game->getBoard().size(); a++)
+	for (int x = 0; x < this->game->getBoard().size(); x++)
 	{
-		if (this->game->getBoard()[a]->valadateMove(this->game->getBoard()[k]->getX(), this->game->getBoard()[k]->getY()) == VALID && this->game->getBoard()[a]->getColor() != this->game->getBoard()[k]->getColor())//the move made a check on the opponent
+		if (this->game->getBoard()[x]->valadateMove(this->game->getBoard()[k]->getX(), this->game->getBoard()[k]->getY()) == VALID && this->game->getBoard()[x]->getColor() != this->game->getBoard()[k]->getColor())//the move made a check on the opponent
 		{
 			res = CHECK;
 		}
@@ -131,14 +123,14 @@ int Board::valadate(int x, int y, int newX, int newY)
 
 	if (res == CHECK || res == CHECKMATE || res == VALID)
 	{
-		if (j != this->game->getBoard().size() && this->game->getBoard()[j]->getColor() != this->game->getBoard()[i]->getColor())// there is a piece of the opponent in the destination
+		if (j != this->game->getBoard().size()&& this->game->getBoard()[j]->getColor() != this->game->getBoard()[i]->getColor())// there is a piece of the opponent in the destination
 		{
 			//delete this->game->getBoard()[j];
 			this->game->getBoard().erase(this->game->getBoard().begin() + j);
 		}
-		this->game->setIsWhiteTurn((!this->game->getIsWhiteTurn()));// setting the turn to the other player
+		this->game->setIsWhiteTurn(!(this->game->getIsWhiteTurn()));// setting the turn to the other player
 	}
-
+	
 	return res;
 }
 GameStatus* Board::getGame() const
