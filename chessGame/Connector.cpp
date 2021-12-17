87223c9c71047080ba748a43c2864f25df0bd154
Gali Yefet <yefetg1967@gmail.com>
2021-12-17 16:20:12 +0200

    main and connector

--- chessGame/Connector.cpp
+++ chessGame/Connector.cpp
@@ -1,30 +1,32 @@
 #include "Connector.h"
 Connector::Connector()
 {
-	_p = Pipe();
-	_game = Board();
-	_p.connect();
-	char c[1024];
-	const char* c = _game.getGame().toString().c_str();
-	_p.sendMessageToGraphics(c);
+	_p = new Pipe();
+	_game = new Board();
+	_p->connect();
+	string c = _game->getGame()->toString();
+	char char_array[1024];
+	strcpy_s(char_array, c.c_str());
+	_p->sendMessageToGraphics(char_array);
 }
 bool Connector::turn()
 {
-	string msg = _p.getMessageFromGraphics();
+	string msg = _p->getMessageFromGraphics();
 	//1a3d
 	int values[4];
 	values[0] = (int)msg[0]-'0';
 	values[1] = (int)msg[0] - 'a'+1;
 	values[2] = (int)msg[0] - '0';
 	values[3] = (int)msg[0] - 'a' + 1;
-	int replay = _game.valadate(values[0], values[1], values[2], values[3]);
+	int replay = _game->valadate(values[0], values[1], values[2], values[3]);
+	cout << replay;
 	char c[2];
 	c[0] = replay + '0';
 	c[1] = NULL;
-	_p.sendMessageToGraphics(c);
+	_p->sendMessageToGraphics(c);
 	return replay == 0 || replay == 1;
 }
 Connector::~Connector()
 {
-	_p.close();
+	_p->close();
 }
